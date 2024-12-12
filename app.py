c5ede7e53e8bb2aba60ef31d1a5ffc9ceacd908f
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2024-12-12 09:45:28 +0700

    emergency

--- app.py
+++ app.py
@@ -1,9 +1,71 @@
+import os
+
+import cv2 as cv
 import gradio as gr
 import numpy as np
-import cv2 as cv
 
-def magic(): 
-    return
+
+def extract_frames(in_dirpath, out_dirpath): 
+    if not os.path.exists(out_dirpath): 
+        os.makedirs(out_dirpath)
+
+    videos = os.listdir(in_dirpath)
+
+    frame_count = 0
+    for filename in videos: 
+        capture = cv.VideoCapture(os.path.join(in_dirpath, filename))
+        
+        while True: 
+            ret, frame = capture.read()
+
+            if not ret: 
+                break
+
+            cv.imwrite(os.path.join(out_dirpath, f'{frame_count:04d}.png'), frame)
+            frame_count += 1
+
+        capture.release()
+
+def classify(dirpath, out_dirpath): 
+    positive_dirpath = os.path.join(out_dirpath, 'positive' )
+    negative_dirpath = os.path.join(out_dirpath, 'negative' )
+    cc = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_fullbody.xml')
+
+    if not os.path.exists(positive_dirpath): 
+        os.makedirs(positive_dirpath)
+
+    if not os.path.exists(negative_dirpath): 
+        os.makedirs(negative_dirpath)
+
+    images = os.listdir(dirpath)
+    for filename in images: 
+        image = cv.imread(os.path.join(dirpath, filename), 0)
+        obj = cc.detectMultiScale(image, 1.05, 3)
+
+        if len(obj) < 1: 
+            cv.imwrite(os.path.join(negative_dirpath, filename),
+                       cv.cvtColor(image, cv.COLOR_GRAY2RGB))
+
+        # an object (fullbody) is detected. draw a bounding box 
+        # around it
+        for (x, y, w, h) in obj: 
+            cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        
+        cv.imwrite(os.path.join(positive_dirpath, filename),
+                   cv.cvtColor(image, cv.COLOR_GRAY2RGB))
+
+def magic(frame, algo): 
+    color = (255, 255, 0)
+    thickness = 10
+    start = (300, 400)
+    end = (800, 600)
+    frame = np.array(frame)
+    cv.rectangle(frame, start, end, color, thickness)
+    cv.imshow('iframe', frame)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return frame
+
 
 with gr.Blocks() as demo: 
     with gr.Row(): 
@@ -20,6 +82,9 @@ with gr.Blocks() as demo:
                 btn_3 = gr.Button(value='Papan 3')
                 btn_4 = gr.Button(value='Dosen')
 
+                upscale_algorithm = gr.Dropdown(choices=['linear', 'cubic', 'none'], 
+                                                value='none', label='Upscaling Algorithm')
+
             output_img = gr.Image(streaming=True)
 
         '''
@@ -33,6 +98,11 @@ with gr.Blocks() as demo:
         # input_image.stream(lambda f, m: f, [input_image, model_picker], [output_img], 
         #                    stream_every=0.0416666667, concurrency_limit=24)
 
-        input_image.stream(lambda f, m: f, [input_image, model_picker], [output_img], 
+        input_image.stream(magic, [input_image, upscale_algorithm], [output_img], 
                            stream_every=0.0417083750,  concurrency_limit=24)
-demo.launch()
+if __name__ == "__main__": 
+    demo.launch()
+    # extract_frames('./dataset/videos/', './dataset/images/')
+    # classify('./dataset/images/', './dataset')
+    pass
+
